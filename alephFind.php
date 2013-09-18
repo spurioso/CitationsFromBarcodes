721650d28639419c32bd71a57afa6e09c503a09e
spurioso <spurioso@gmail.com>
2013-09-18 14:17:49 -0400

    Added style to change output font to Calibri.  * added style.css * referenced css in alephBarcodeForm.html * stripped tags in citation in alephFind.php. Fixes #3.

--- alephFind.php
+++ alephFind.php
@@ -13,8 +13,8 @@ $values = readSettingsFile(); */
 
 
 $barcodes = array ("31430054272036","31430054447695","31430055485397","31430055203543","31430030940326", "31430049752605", "31430052399393", "31430052399393", "31430056673173", "31430042466138","31430006677696","31430007712559","31430006129102","31430053093094","31430039849874","31430054128535"); //barcodes for testing
-//$barcode = $barcodes[6];
-$barcode = $_REQUEST["barcode"]; //get barcode from alephBarcodeForm.html form
+$barcode = $barcodes[6];
+//$barcode = $_REQUEST["barcode"]; //get barcode from alephBarcodeForm.html form
 $style = $_REQUEST["style"];
 
 $op = "find";
@@ -89,7 +89,12 @@ echo "<strong>ISBN: </strong>".$isbn."<br />";
 echo "<strong>Barcode: </strong>".$barcode."<br />";
 echo "<a href=\"".$alephURL."\" target=\"_blank\">".$alephURL."</a><br /><br />";
 echo "<a href=\"".$permalink."\" target=\"_blank\">".$permalink."</a><br />"; //print out the WCL permalink
-echo $citation; //print out the citation
+echo "</br>";
+
+//print out the citation, stripping all HTML tags but <em> and <i>, necessary for CSS formatting on AlephBarcodeForm.html
+echo strip_tags($citation, "<em><i>");
+
+echo "</br></br>";
 
 foreach ($collegeParkLocations as $location) {  //print out holding info for each College Park item
 echo $location["sublibrary"]."<br />";
